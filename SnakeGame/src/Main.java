96e5fcd7aaaf3012f115b2747f6a61c3bdaf778c
lena73230 <43807651+lena73230@users.noreply.github.com>
2019-01-27 00:26:17 +0100

    Add files via upload

---
+++ SnakeGame/src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Main {
+
+    public Main (){
+
+        JFrame frame = new JFrame();
+        GamePanel gamepanel = new GamePanel();
+
+        frame.add(gamepanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SNAKECODING");
+
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        new Main();
+    }
+
+}
